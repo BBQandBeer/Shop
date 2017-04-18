0fd5739b71739d38814c3eae227a61a19ccd4b21
DDaskalov <shlomo7492@yahoo.com>
2017-04-18 23:25:38 +0300

    Sales view functionality commit

--- Shop/ViewOrders.xaml.cs
+++ Shop/ViewOrders.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer;
+using DomainClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,13 +17,47 @@ using System.Windows.Shapes;
 namespace Shop
 {
     /// <summary>
-    /// Interaction logic for ViewOrders.xaml
+    /// Interaction logic for VewOrders.xaml
     /// </summary>
     public partial class ViewOrders : Window
     {
+        private ShopContext vOrder = new ShopContext();
+
         public ViewOrders()
         {
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+
             InitializeComponent();
+            List<DataSourceItem> dataSource = new List<DataSourceItem>();
+            List<Order> orderList = vOrder.Orders.ToList();
+            foreach (var ord in orderList)
+            {
+                DataSourceItem newItem = new DataSourceItem();
+                newItem.OrderId = ord.OrderId;
+                newItem.OrderDateTime = ord.OrderDateTime;
+                newItem.OrderedProducts = "";
+                newItem.TotalPrice = 0m;
+                List<OrderedProduct> orderedProducts = vOrder.OrderedProducts
+                                                             .Where(o => o.OrderID == ord.OrderId)
+                                                             .ToList();
+                foreach (var op in orderedProducts)
+                {
+                    Product prod = vOrder.Products
+                                         .Where(p => p.ProductId == op.ProductID)
+                                         .SingleOrDefault();
+                    newItem.OrderedProducts += prod.Description + " - " + op.Quantity + "x" + prod.Price + "лв." + ";\n ";
+                    newItem.TotalPrice += (op.Quantity * prod.Price);
+                }
+                dataSource.Add(newItem);
+            }
+            dataGrid.ItemsSource = dataSource;
+        }
+        public class DataSourceItem
+        {
+            public int OrderId { get; set; }
+            public DateTime OrderDateTime { get; set; }
+            public string OrderedProducts { get; set; }
+            public decimal TotalPrice { get; set; }
         }
     }
 }
